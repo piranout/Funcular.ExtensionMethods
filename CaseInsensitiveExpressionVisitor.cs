ccdc4d3aa9a14b11d9c6735dbaf144ba4fd92cb4
Paul Smith <ps+github@ps.cm>
2015-07-08 17:35:20 -0500

    Start Expression Extensions development

---
+++ CaseInsensitiveExpressionVisitor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Linq.Expressions;
+
+namespace Funcular.ExtensionMethods
+{
+    public class CaseInsensitiveExpressionVisitor : ExpressionVisitor
+    {
+        protected override Expression VisitBinary(BinaryExpression b)
+        {
+            // if (b.NodeType == ExpressionType.AndAlso)
+            if (b.NodeType == ExpressionType.Equal && b.Left.Type == typeof(string) && b.Right.Type == typeof(string))
+            {
+                Expression left = this.Visit(b.Left);
+                Expression right = this.Visit(b.Right);
+
+                // Make this binary expression an OrElse operation instead of an AndAlso operation. 
+                // PredicateBuilder.False<string>().Or((string s) => s.Equals)
+                return Expression.MakeBinary(ExpressionType.OrElse, left, right, b.IsLiftedToNull, b.Method);
+            }
+
+            return base.VisitBinary(b);
+        }
+    }
+}
---
+++ UnitTests/ExpressionTests.cs
@@ -0,0 +1,15 @@
+﻿using Funcular.ExtensionMethods;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ExpressionTests
+    {
+        [TestMethod]
+        public void Converted_Expression_Is_Case_Insensitive()
+        {
+            Assert.IsTrue(" ".HasValue());
+        }
+    }
+}
